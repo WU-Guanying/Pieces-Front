6505c685a39ca76a9829ddac5e64558aacc0c540
WU Guanying <guanying.x.wu@gmail.com>
2025-04-11 20:54:36 +0800

    update front

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -941,7 +941,7 @@ const UserProfile = () => {
                                     className={`confetti ${getRandomShape()} ${getRandomColor()}`}
                                     style={{
                                         left: `${Math.random() * 90}%`,
-                                        top: `${Math.random() * -200}px`,
+                                        top: `${Math.random() * 10}px`,
                                         animationDelay: `${Math.random() * 2}s`,
                                         '--float-x': floatX,
                                         '--drift-x': driftX,
